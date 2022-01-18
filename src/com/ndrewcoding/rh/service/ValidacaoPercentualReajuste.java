a7b7b7436328a380e5f6567dcca927b89cfce0f5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-17 21:28:07 -0300

    Feat: implement method validar() of ValidacaoPercentualReajuste

--- src/com/ndrewcoding/rh/service/ValidacaoPercentualReajuste.java
+++ src/com/ndrewcoding/rh/service/ValidacaoPercentualReajuste.java
@@ -1,4 +1,19 @@
 package com.ndrewcoding.rh.service;
 
+import com.ndrewcoding.rh.ValidacaoException;
+import com.ndrewcoding.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class ValidacaoPercentualReajuste {
+
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal salarioAtual = funcionario.getSalario();
+        BigDecimal percentualDeReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        if (percentualDeReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste salarial não pode ser superior à 40%!");
+        }
+    }
+
 }

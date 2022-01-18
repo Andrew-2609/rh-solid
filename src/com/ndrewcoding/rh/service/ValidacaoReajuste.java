e4274d377d9519cd164d245c97f5e22c899093e3
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-17 21:30:48 -0300

    Feat: create method validar() inside ValidacaoReajuste interface

--- src/com/ndrewcoding/rh/service/ValidacaoReajuste.java
+++ src/com/ndrewcoding/rh/service/ValidacaoReajuste.java
@@ -1,4 +1,11 @@
 package com.ndrewcoding.rh.service;
 
+import com.ndrewcoding.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+
 public interface ValidacaoReajuste {
+
+    void validar(Funcionario funcionario, BigDecimal aumento);
+
 }

d926f4c8507882461a8626be03c29fa505642916
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-17 21:29:32 -0300

    Feat: implement method validar() of ValidacaoPeriodicidadeEntreReajustes

--- src/com/ndrewcoding/rh/service/ValidacaoPeriodicidadeEntreReajustes.java
+++ src/com/ndrewcoding/rh/service/ValidacaoPeriodicidadeEntreReajustes.java
@@ -1,4 +1,21 @@
 package com.ndrewcoding.rh.service;
 
+import com.ndrewcoding.rh.ValidacaoException;
+import com.ndrewcoding.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class ValidacaoPeriodicidadeEntreReajustes {
+
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+        LocalDate dataDoUltimoReajuste = funcionario.getDataDoUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDesteOUltimoReajuste = ChronoUnit.MONTHS.between(dataDoUltimoReajuste, dataAtual);
+        if (mesesDesteOUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes salariais não podem ser menores que 6 meses!");
+        }
+    }
+
 }

2f6dc618c3599c57d6cf724dfc8b478af6c23f2c
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-18 22:37:56 -0300

    Feat: implement method promover() of PromocaoService

--- src/com/ndrewcoding/rh/service/promocao/PromocaoService.java
+++ src/com/ndrewcoding/rh/service/promocao/PromocaoService.java
@@ -1,4 +1,23 @@
 package com.ndrewcoding.rh.service.promocao;
 
+import com.ndrewcoding.rh.ValidacaoException;
+import com.ndrewcoding.rh.model.Cargo;
+import com.ndrewcoding.rh.model.Funcionario;
+
 public class PromocaoService {
+
+    public void promover(Funcionario funcionario, boolean metaAtingida) {
+        Cargo cargoAtual = funcionario.getCargo();
+        if (Cargo.GERENTE == cargoAtual) {
+            throw new ValidacaoException("Gerentes não podem ser promovidos!");
+        }
+
+        if (!metaAtingida) {
+            throw new ValidacaoException("Funcionário não bateu a meta!");
+        }
+
+        Cargo novoCargo = cargoAtual.getProximoCargo();
+        funcionario.promover(novoCargo);
+    }
+
 }

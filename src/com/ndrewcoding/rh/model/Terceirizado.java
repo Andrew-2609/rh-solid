7e819c7b7212cd6dd9a67febbdff21cfb41c3ad3
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-20 20:14:00 -0300

    Feat: add basic structure of class Terceirizado

--- src/com/ndrewcoding/rh/model/Terceirizado.java
+++ src/com/ndrewcoding/rh/model/Terceirizado.java
@@ -1,4 +1,27 @@
 package com.ndrewcoding.rh.model;
 
+import java.math.BigDecimal;
+
 public class Terceirizado {
+
+    private final DadosPessoais dadosPessoais;
+    private String empresa;
+
+    public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario, String empresa) {
+        this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+        this.empresa = empresa;
+    }
+
+    public DadosPessoais getDadosPessoais() {
+        return dadosPessoais;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
 }

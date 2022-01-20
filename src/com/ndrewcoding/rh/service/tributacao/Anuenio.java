7636721b7d661dc3dabf327834d9ce74acea7867
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-20 20:27:37 -0300

    Feat: add basic implementation of Anuenio

--- src/com/ndrewcoding/rh/service/tributacao/Anuenio.java
+++ src/com/ndrewcoding/rh/service/tributacao/Anuenio.java
@@ -1,4 +1,26 @@
 package com.ndrewcoding.rh.service.tributacao;
 
-public class Anuenio {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Anuenio implements Reajuste {
+
+    private final BigDecimal valor;
+    private final LocalDate data;
+
+    public Anuenio(BigDecimal valor, LocalDate data) {
+        this.valor = valor;
+        this.data = data;
+    }
+
+    @Override
+    public BigDecimal valor() {
+        return valor;
+    }
+
+    @Override
+    public LocalDate data() {
+        return data;
+    }
+
 }

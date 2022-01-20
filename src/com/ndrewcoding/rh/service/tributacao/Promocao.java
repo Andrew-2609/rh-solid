072e92a27e4ace3698b7a43abcd3911cf601b98a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-20 20:28:34 -0300

    Feat: add basic implementation of Promocao

--- src/com/ndrewcoding/rh/service/tributacao/Promocao.java
+++ src/com/ndrewcoding/rh/service/tributacao/Promocao.java
@@ -1,4 +1,26 @@
 package com.ndrewcoding.rh.service.tributacao;
 
-public class Promocao {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Promocao implements Reajuste {
+
+    private final BigDecimal valor;
+    private final LocalDate data;
+
+    public Promocao(BigDecimal valor, LocalDate data) {
+        this.valor = valor;
+        this.data = data;
+    }
+
+    @Override
+    public BigDecimal valor() {
+        return valor;
+    }
+
+    @Override
+    public LocalDate data() {
+        return data;
+    }
+
 }
